658051fcd0e189bda52872c9292502f8a4655a0b
giulio-natangelo <155229670+giulio-natangelo@users.noreply.github.com>
2023-12-31 12:01:20 +0100

    direction + projectile

--- main.ts
+++ main.ts
@@ -1,4 +1,318 @@
-let myDuck = sprites.create(img`
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, myDuck)
+    music.play(music.createSoundEffect(WaveShape.Sine, 2481, 0, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
+    if (direction == "right") {
+        myDuck.setImage(img`
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 b c 5 5 d 4 c . . 
+            . b b b b 5 5 5 b f d d 4 4 4 b 
+            . b d 5 b 5 5 b c b 4 4 4 4 b . 
+            . . b 5 5 b 5 5 5 4 4 4 4 b . . 
+            . . b d 5 5 b 5 5 5 5 5 5 b . . 
+            . b d b 5 5 5 d 5 5 5 5 5 5 b . 
+            b d d c d 5 5 b 5 5 5 5 5 5 b . 
+            c d d d c c b 5 5 5 5 5 5 5 b . 
+            c b d d d d d 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 4 . . . . . . . 
+            . . . . . . 4 5 5 4 . . . . . . 
+            . . . . . . 2 5 5 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, myDuck, 150, 0)
+    } else if (direction == "left") {
+        myDuck.setImage(img`
+            . . . . b 5 b . . . . . . . . . 
+            . . . . b 5 b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . c 4 d 5 5 c b 5 b b . . . . 
+            b 4 4 4 d d f b 5 5 5 b b b b . 
+            . b 4 4 4 4 b c b 5 5 b 5 d b . 
+            . . b 4 4 4 4 5 5 5 b 5 5 b . . 
+            . . b 5 5 5 5 5 5 b 5 5 d b . . 
+            . b 5 5 5 5 5 5 d 5 5 5 b d b . 
+            . b 5 5 5 5 5 5 b 5 5 d c d d b 
+            . b 5 5 5 5 5 5 5 b c c d d d c 
+            . b 5 5 5 5 5 5 5 d d d d d b c 
+            . b d 5 5 5 5 5 d d d d d d c . 
+            . . b b 5 5 5 d d d d d b c . . 
+            . . . b b c c c c c c c c . . . 
+            `)
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 4 . . . . . . . 
+            . . . . . . 4 5 5 4 . . . . . . 
+            . . . . . . 2 5 5 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, myDuck, -150, 0)
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    direction = "left"
+    setDuckAnimation()
+})
+function setDuckAnimation () {
+    if (direction == "right") {
+        animation.runImageAnimation(
+        myDuck,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . b 5 5 b . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . b b b b b 5 5 5 5 5 5 5 b . . 
+            . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+            . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+            . . b d 5 5 b 1 f f 5 4 4 c . . 
+            b b d b 5 5 5 d f b 4 4 4 4 b . 
+            b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+            c d d d c c b 5 5 5 5 5 5 5 b . 
+            c b d d d d d 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . b b b b b 5 5 5 5 5 5 5 b . . 
+            . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+            . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+            . . b d 5 5 b 1 f f 5 4 4 c . . 
+            b b d b 5 5 5 d f b 4 4 4 4 4 b 
+            b d d c d 5 5 b 5 4 4 4 4 4 b . 
+            c d d d c c b 5 5 5 5 5 5 5 b . 
+            c b d d d d d 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `,img`
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . . . . b c . . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 5 d f . . 
+            . . . . b 5 5 1 f f 5 d 4 c . . 
+            . . . . b 5 5 d f b d d 4 4 . . 
+            b d d d b b d 5 5 5 4 4 4 4 4 b 
+            b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+            b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+            c d d c d 5 5 b 5 5 5 5 5 5 b . 
+            c b d d c c b 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `,img`
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 d 4 c . . 
+            . . . . b 5 5 1 f f d d 4 4 4 b 
+            . . . . b 5 5 d f b 4 4 4 4 b . 
+            . . . b d 5 5 5 5 4 4 4 4 b . . 
+            . . b d d 5 5 5 5 5 5 5 5 b . . 
+            . b d d d d 5 5 5 5 5 5 5 5 b . 
+            b d d d b b b 5 5 5 5 5 5 5 b . 
+            c d d b 5 5 d c 5 5 5 5 5 5 b . 
+            c b b d 5 d c d 5 5 5 5 5 5 b . 
+            . b 5 5 b c d d 5 5 5 5 5 d b . 
+            b b c c c d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            `,img`
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 d 4 c . . 
+            . . . . b 5 5 1 f f d d 4 4 4 b 
+            . . . . b 5 5 d f b 4 4 4 4 b . 
+            . . . b d 5 5 5 5 4 4 4 4 b . . 
+            . b b d d d 5 5 5 5 5 5 5 b . . 
+            b d d d b b b 5 5 5 5 5 5 5 b . 
+            c d d b 5 5 d c 5 5 5 5 5 5 b . 
+            c b b d 5 d c d 5 5 5 5 5 5 b . 
+            c b 5 5 b c d d 5 5 5 5 5 5 b . 
+            b b c c c d d d 5 5 5 5 5 d b . 
+            . . . . c c d d d 5 5 5 b b . . 
+            . . . . . . c c c c c b b . . . 
+            `,img`
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . . . . b b 5 d 1 f 5 5 d f . . 
+            . . . . b 5 5 1 f f 5 d 4 c . . 
+            . . . . b 5 5 d f b d d 4 4 . . 
+            . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+            b d d d b b d 5 5 4 4 4 4 4 b . 
+            b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+            c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+            c b d c d 5 5 b 5 5 5 5 5 5 b . 
+            . c d d c c b d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        200,
+        true
+        )
+    } else if (direction == "left") {
+        animation.runImageAnimation(
+        myDuck,
+        [img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . b 5 5 b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . b 5 5 5 5 5 5 5 b b b b b . 
+            . . b 5 5 5 5 5 5 5 5 b 5 d b . 
+            . . f 4 d 5 f 1 d 5 b 5 5 b . . 
+            . . c 4 4 5 f f 1 b 5 5 d b . . 
+            . b 4 4 4 4 b f d 5 5 5 b d b b 
+            b 4 4 4 4 4 4 5 b 5 5 d c d d b 
+            . b 5 5 5 5 5 5 5 b c c d d d c 
+            . b 5 5 5 5 5 5 5 d d d d d b c 
+            . b d 5 5 5 5 5 d d d d d d c . 
+            . . b b 5 5 5 d d d d d b c . . 
+            . . . b b c c c c c c c c . . . 
+            `,img`
+            . . . . . . . . . . . . . . . . 
+            . . . b 5 b . . . . . . . . . . 
+            . . . . b 5 b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . b 5 5 5 5 5 5 5 b b b b b . 
+            . . b 5 5 5 5 5 5 5 5 b 5 d b . 
+            . . f 4 d 5 f 1 d 5 b 5 5 b . . 
+            . . c 4 4 5 f f 1 b 5 5 d b . . 
+            b 4 4 4 4 4 b f d 5 5 5 b d b b 
+            . b 4 4 4 4 4 5 b 5 5 d c d d b 
+            . b 5 5 5 5 5 5 5 b c c d d d c 
+            . b 5 5 5 5 5 5 5 d d d d d b c 
+            . b d 5 5 5 5 5 d d d d d d c . 
+            . . b b 5 5 5 d d d d d b c . . 
+            . . . b b c c c c c c c c . . . 
+            `,img`
+            . . . b 5 b . . . . . . . . . . 
+            . . . . b 5 b . . . . . . . . . 
+            . . . . . c b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . f d 5 5 f 1 d 5 b b . . . . 
+            . . c 4 d 5 f f 1 5 5 b . . . . 
+            . . 4 4 d d b f d 5 5 b . . . . 
+            b 4 4 4 4 4 5 5 5 d b b d d d b 
+            . b 4 4 4 4 4 5 5 b 5 5 5 d b b 
+            . . b 5 5 5 5 5 d 5 5 5 5 c d b 
+            . b 5 5 5 5 5 5 b 5 5 d c d d c 
+            . b 5 5 5 5 5 5 5 b c c d d b c 
+            . b d 5 5 5 5 5 d d d d d d c . 
+            . . b b 5 5 5 d d d d d b c . . 
+            . . . b b c c c c c c c c . . . 
+            `,img`
+            . . . b 5 b . . . . . . . . . . 
+            . . . . b 5 b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . c 4 d 5 f 1 d 5 b b . . . . 
+            b 4 4 4 d d f f 1 5 5 b . . . . 
+            . b 4 4 4 4 b f d 5 5 b . . . . 
+            . . b 4 4 4 4 5 5 5 5 d b . . . 
+            . . b 5 5 5 5 5 5 5 5 d d b . . 
+            . b 5 5 5 5 5 5 5 5 d d d d b . 
+            . b 5 5 5 5 5 5 5 b b b d d d b 
+            . b 5 5 5 5 5 5 c d 5 5 b d d c 
+            . b 5 5 5 5 5 5 d c d 5 d b b c 
+            . b d 5 5 5 5 5 d d c b 5 5 b . 
+            . . b b 5 5 5 d d d d c c c b b 
+            . . . b b c c c c c c c c . . . 
+            `,img`
+            . . . b 5 b . . . . . . . . . . 
+            . . . . b 5 b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . c 4 d 5 f 1 d 5 b b . . . . 
+            b 4 4 4 d d f f 1 5 5 b . . . . 
+            . b 4 4 4 4 b f d 5 5 b . . . . 
+            . . b 4 4 4 4 5 5 5 5 d b . . . 
+            . . b 5 5 5 5 5 5 5 d d d b b . 
+            . b 5 5 5 5 5 5 5 b b b d d d b 
+            . b 5 5 5 5 5 5 c d 5 5 b d d c 
+            . b 5 5 5 5 5 5 d c d 5 d b b c 
+            . b 5 5 5 5 5 5 d d c b 5 5 b c 
+            . b d 5 5 5 5 5 d d d c c c b b 
+            . . b b 5 5 5 d d d c c . . . . 
+            . . . b b c c c c c . . . . . . 
+            `,img`
+            . . . b 5 b . . . . . . . . . . 
+            . . . . b 5 b . . . . . . . . . 
+            . . . . b b b b b b . . . . . . 
+            . . . b 5 5 5 5 5 b b . . . . . 
+            . . f d 5 5 f 1 d 5 b b . . . . 
+            . . c 4 d 5 f f 1 5 5 b . . . . 
+            . . 4 4 d d b f d 5 5 b . . . . 
+            b 4 4 4 4 4 5 5 5 5 5 d b b b . 
+            . b 4 4 4 4 4 5 5 d b b d d d b 
+            . . b 5 5 5 5 5 5 b 5 5 5 d b b 
+            . b 5 5 5 5 5 5 d 5 5 5 5 c d c 
+            . b 5 5 5 5 5 5 b 5 5 d c d b c 
+            . b d 5 5 5 5 5 d b c c d d c . 
+            . . b b 5 5 5 d d d d d b c . . 
+            . . . b b c c c c c c c c . . . 
+            . . . . . . . . . . . . . . . . 
+            `],
+        200,
+        true
+        )
+    }
+}
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    direction = "right"
+    setDuckAnimation()
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    setDuckAnimation()
+})
+let projectile: Sprite = null
+let direction = ""
+let myDuck: Sprite = null
+myDuck = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . b 5 5 b . . . 
@@ -139,6 +453,7 @@ scene.setBackgroundImage(img`
     99999999999996666666666666677b6666666666666697777669999977777777777777777999999999999999999996666666666666677b66666666666666977776699999777777777777777779999999
     9999999999999666666666666666666666666666667777777769999999999999999999999999999999999999999996666666666666666666666666666677777777699999999999999999999999999999
     `)
+direction = "right"
 animation.runImageAnimation(
 myDuck,
 [img`
